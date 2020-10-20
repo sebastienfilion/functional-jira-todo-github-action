e6bb22b06d056e27c2b38244fc1ade35f9654526
Sebastien Filion <sebastienfilion@mac.com>
2020-10-20 08:37:34 -0400

    Create simple action

---
+++ action.js
@@ -0,0 +1,48 @@
+import {
+  both,
+  complement,
+  compose,
+  curry,
+  filter,
+  lift,
+  map,
+  match,
+  prop,
+  reduce,
+  split,
+  test,
+  trim,
+  useWith,
+  when
+} from "https://x.nest.land/ramda@0.27.0/source/index.js";
+
+import Either from "https://deno.land/x/functional@v1.0.0/library/Either.js";
+import Task from "https://deno.land/x/functional@v1.0.0/library/Task.js";
+
+const parseCodeDifference = options => map(
+  compose(
+    map(
+      compose(
+        when(
+          both(
+            test(/^\+\s*\/\/\s*TODO\:\s*(.+)/),
+            complement(test(new RegExp(`${options.jiraTicketPrefix}-[0-9]+`)))
+          ),
+          text => {
+            throw new Error(`Could not determine the Jira ticket number for new TODO: "${text}".`)
+          }
+        )
+      )
+    ),
+    split(/\n/),
+    x => new TextDecoder().decode(x)
+  )
+);
+
+const container = await parseCodeDifference({ jiraTicketPrefix: Deno.env.get("JIRA_TICKET_PREFIX") })
+  (Task.wrap(_ => Deno.readAll(Deno.stdin))).run();
+
+if (Either.Left.is(container)) {
+  console.error(container[Symbol.for("Value")]);
+  Deno.exit(1);
+}
